42a75260c7929f3591e391c243adb09a6bf508b9
Joachim Toft <joachim.toft@hey.com>
2020-12-06 16:42:35 +0100

    day 6

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -1,25 +1,59 @@
 use crate::solution::Solution;
+use std::collections::HashMap;
 
 pub struct Day;
 
 impl Solution for Day {
-    type Input = Vec<String>;
-    type Output1 = i32;
-    type Output2 = i32;
+    type Input = Vec<Vec<String>>;
+    type Output1 = usize;
+    type Output2 = usize;
 
     fn get_input_file_path(&self) -> String {
         "input/day06".to_string()
     }
 
-    fn parse_input(&self, puzzle_input: String) -> Vec<String> {
-        puzzle_input.lines().map(str::to_string).collect()
+    fn parse_input(&self, puzzle_input: String) -> Vec<Vec<String>> {
+        puzzle_input
+            .split("\n\n")
+            .map(|group| group.split("\n").map(str::to_string).collect())
+            .collect()
     }
 
-    fn part1(&self, input: &Vec<String>) -> i32 {
-        1
+    fn part1(&self, input: &Vec<Vec<String>>) -> usize {
+        input
+            .iter()
+            .map(|group| {
+                let mut questions: HashMap<char, u32> = HashMap::new();
+
+                for answers in group {
+                    for answer in answers.chars() {
+                        let count = questions.get(&answer).map_or(1, |val| val + 1);
+                        questions.insert(answer, count);
+                    }
+                }
+                questions.keys().len()
+            })
+            .sum()
     }
 
-    fn part2(&self, input: &Vec<String>) -> i32 {
-        1
+    fn part2(&self, input: &Vec<Vec<String>>) -> usize {
+        input
+            .iter()
+            .map(|group| {
+                let mut questions: HashMap<char, usize> = HashMap::new();
+
+                for answers in group {
+                    for answer in answers.chars() {
+                        let count = questions.get(&answer).map_or(1, |val| val + 1);
+                        questions.insert(answer, count);
+                    }
+                }
+                questions
+                    .values()
+                    .filter(|v| **v == group.len())
+                    .collect::<Vec<_>>()
+                    .len()
+            })
+            .sum()
     }
 }
